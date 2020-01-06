46217827b95aa339537d30a351940520c605f49b
Petter Bøe Hørtvedt <petterho@nmbu.no>
2020-01-06 17:02:59 +0100

    Added simulation tests

---
+++ tests/test_simulation.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+__author__ = "Jon-Mikkel Korsvik & Petter Bøe Hørtvedt"
+__email__ = "jonkors@nmbu.no & petterho@nmbu.no"
+
+
+class TestSimulation:
+    def test_set_animal_parameters(self):
+        raise NotImplementedError
+
+    def test_set_landscape_parameters(self):
+        raise NotImplementedError
+
+    def test_simulate(self):
+        raise NotImplementedError
+
+    def test_add_population(self):
+        raise NotImplementedError
+
+    def test_year(self):
+        raise NotImplementedError
+
+    def test_num_animals(self):
+        raise NotImplementedError
+
+    def test_num_animals_per_species(self):
+        raise NotImplementedError
+
+    def test_animal_distribution(self):
+        raise NotImplementedError
+
+    def test_make_movie(self):
+        raise NotImplementedError
