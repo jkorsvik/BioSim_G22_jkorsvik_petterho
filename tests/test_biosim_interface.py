770ac3ce33ca52c7521d5d38839671461dcbb517
Petter Bøe Hørtvedt <petterho@nmbu.no>
2020-01-06 17:46:37 +0100

    Fixed the import-statement

--- tests/test_biosim_interface.py
+++ tests/test_biosim_interface.py
@@ -9,9 +9,10 @@ the simulation module of the biosim package.
 Notes:
      - The BioSim class should pass all tests in this set.
      - The tests check only that the class interface can be used, not that
-       the class functions correctly. You need to write your own tests for that.
-     - You should only run these tests on your code *after* you have implemented
-       both animal and all landscape classes.
+       the class functions correctly. You need to write your own tests
+       for that.
+     - You should only run these tests on your code *after* you have
+       implemented both animal and all landscape classes.
 """
 
 __author__ = "Hans Ekkehard Plesser"
@@ -24,7 +25,7 @@ import glob
 import os
 import os.path
 
-from biosim.simulation import BioSim
+from src.biosim.simulation import BioSim
 
 
 def test_empty_island():
@@ -247,7 +248,8 @@ def test_set_plot_limits():
 
 @pytest.fixture
 def figfile_root():
-    """Provide name for figfile root and delete figfiles after test completes"""
+    """Provide name for figfile root and delete figfiles after test
+    completes"""
 
     ffroot = os.path.join(".", "testfigroot")
     yield ffroot
