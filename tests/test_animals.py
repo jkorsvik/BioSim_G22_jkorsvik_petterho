5b1cb476bf3019560c07dd1106a191f3c6a7942e
Petter Bøe Hørtvedt <petterho@nmbu.no>
2020-01-06 16:31:46 +0100

    Added files for testing animals and landscape

---
+++ tests/test_animals.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+__author__ = "Jon-Mikkel Korsvik & Petter Bøe Hørtvedt"
+__email__ = "jonkors@nmbu.no & petterho@nmbu.no"
+
+
+class TestAnimal:
+    raise NotImplementedError
+
+
+class TestHerbivore:
+    raise NotImplementedError
+
+
+class TestCarnivore:
+    raise NotImplementedError
---
+++ tests/test_landscape.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+__author__ = "Jon-Mikkel Korsvik & Petter Bøe Hørtvedt"
+__email__ = "jonkors@nmbu.no & petterho@nmbu.no"
+
+
+class TestCell:
+    raise NotImplementedError
+
+
+class TestOcean:
+    raise NotImplementedError
+
+
+class TestMountain:
+    raise NotImplementedError
+
+
+class TestDesert:
+    raise NotImplementedError
+
+
+class TestSavanna:
+    raise NotImplementedError
+
+
+class TestJungle:
+    raise NotImplementedError
