865bd85b2a4805b175a3685316289ff4ce98332a
Petter Bøe Hørtvedt <petterho@nmbu.no>
2020-01-06 16:24:08 +0100

    Added classes and methods

---
+++ src/biosim/animals.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+__author__ = "Jon-Mikkel Korsvik & Petter Bøe Hørtvedt"
+__email__ = "jonkors@nmbu.no & petterho@nmbu.no"
+
+
+class Animal:
+    def __init__(self):
+        raise NotImplementedError
+
+    def migration(self):
+        raise NotImplementedError
+
+    def birth(self):
+        raise NotImplementedError
+
+    def death(self):
+        raise NotImplementedError
+
+
+class Herbivore(Animal):
+    def __init__(self):
+        raise NotImplementedError
+
+    def feed(self):
+        raise NotImplementedError
+
+
+class Carnivore(Animal):
+    def __init__(self):
+        raise NotImplementedError
+
+    def feed(self):
+        raise NotImplementedError
---
+++ src/biosim/landscape.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+__author__ = "Jon-Mikkel Korsvik & Petter Bøe Hørtvedt"
+__email__ = "jonkors@nmbu.no & petterho@nmbu.no"
+
+
+class Cell:
+    def __init__(self):
+        raise NotImplementedError
+
+
+class Ocean(Cell):
+    def __init__(self):
+        raise NotImplementedError
+
+
+class Mountain(Cell):
+    def __init__(self):
+        raise NotImplementedError
+
+
+class Desert(Cell):
+    def __init__(self):
+        raise NotImplementedError
+
+
+class Savanna(Cell):
+    def __init__(self):
+        raise NotImplementedError
+
+
+class Jungle(Cell):
+    def __init__(self):
+        raise NotImplementedError
