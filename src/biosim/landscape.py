b366d6479c4e671e019863a9119e315899fcd880
Jon-Mikkel Korscik <jonkors@nmbu.no>
2020-01-09 14:27:41 +0100

    Updated code in landscape, cell.feed_carnivore and cell.feed_herbivore

--- src/biosim/landscape.py
+++ src/biosim/landscape.py
@@ -10,6 +10,10 @@ import textwrap
 from pprint import pprint
 
 
+def sort_by_fitness(animal_list):
+    return animal_list.sort(key=lambda var: var.fitness)
+
+
 class Cell:
     def __init__(self):
         self.herbivores = []
@@ -30,23 +34,23 @@ class Cell:
         pass
 
     @staticmethod
-    def sort_by_fitness(list_of_animals):
-        return list_of_animals.sorted(key=lambda var: var.fitness)
+    def sort_by_fitness(animal_list):
+        return animal_list.sort(key=lambda var: var.fitness)
 
     def feed_all(self):
         self.feed_herbivores()
         self.feed_carnivores()
 
     def feed_herbivores(self):
-        self.herbivores = self.sort_by_fitness(self.herbivores)
+        self.herbivores = sort_by_fitness(self.herbivores)
         for herbivore in reversed(self.herbivores):
-            herbivore.feed(self.fodder)
+            self.fodder = herbivore.feed(self.fodder)
 
     def feed_carnivores(self):
-        self.herbivores = self.sort_by_fitness(self.herbivores)
-        self.carnivores = self.sort_by_fitness(self.carnivores)
+        self.herbivores = sort_by_fitness(self.herbivores)
+        self.carnivores = sort_by_fitness(self.carnivores)
         for carnivore in reversed(self.carnivores):
-            carnivore.feed(self.herbivores)
+            self.herbivores = carnivore.feed(self.herbivores)
 
     @property
     def num_carnivores(self):
