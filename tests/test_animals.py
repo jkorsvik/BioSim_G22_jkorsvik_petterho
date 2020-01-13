166327c268793ad4d7c4b8ffcba7a281186cc88f
Petter Bøe Hørtvedt <petterho@nmbu.no>
2020-01-13 16:44:30 +0100

    Added new tests

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -13,16 +13,6 @@ from pprint import pprint
 
 
 @pytest.fixture
-def carnivore_parameters_right():
-    return {'eta': 0.03, 'phi_age': 0.3, 'DeltaPhiMax': 8.0}
-
-
-@pytest.fixture
-def carnivore_parameters_wrong():
-    return {'zettet': 7}
-
-
-@pytest.fixture
 def herbivore_list():
     list_ = []
     for x in range(10):
@@ -35,7 +25,60 @@ def herbivore_list():
     return herb_list
 
 
+def reset_parameters():
+    herbivore_parameters_right = {'w_birth': 8.0,
+                                  'sigma_birth': 1.5,
+                                  'beta': 0.9,
+                                  'eta': 0.05,
+                                  'a_half': 40.0,
+                                  'phi_age': 0.2,
+                                  'w_half': 10.0,
+                                  'phi_weight': 0.1,
+                                  'mu': 0.25,
+                                  'lambda_': 1.0,
+                                  'gamma': 0.2,
+                                  'zeta': 3.5,
+                                  'xi': 1.2,
+                                  'omega': 0.4,
+                                  'F': 10.0}
+    carnivore_parameters_right = {'w_birth': 6.0,
+                                  'sigma_birth': 1.0,
+                                  'beta': 0.75,
+                                  'eta': 0.125,
+                                  'a_half': 60.0,
+                                  'phi_age': 0.4,
+                                  'w_half': 4.0,
+                                  'phi_weight': 0.4,
+                                  'mu': 0.4,
+                                  'lambda_': 1.0,
+                                  'gamma': 0.8,
+                                  'zeta': 3.5,
+                                  'xi': 1.1,
+                                  'omega': 0.9,
+                                  'F': 50.0,
+                                  'DeltaPhiMax': 10.0}
+    Carnivore.set_parameters(carnivore_parameters_right)
+    Herbivore.set_parameters(herbivore_parameters_right)
+
+
 class TestAnimal:
+    def test_set_parameters(self):
+        animal = Carnivore(30, 10)
+        animal.set_parameters({'eta': 0.03,
+                               'phi_age': 0.3,
+                               'DeltaPhiMax': 8.0})
+        assert animal.eta == 0.03
+        assert animal.phi_age == 0.3
+
+        with pytest.raises(NameError):
+            animal.set_parameters({'zettet': 0.5})
+
+        with pytest.raises(ValueError):
+            animal.set_parameters({'eta': 4})
+
+        reset_parameters()
+        assert animal.eta == 0.125
+
     def test_init(self):
         test_animal = Animal(10, 20)
         assert hasattr(test_animal, 'age')
@@ -61,45 +104,65 @@ class TestAnimal:
         assert abs(sum(delta)) == 1 or sum(delta) == 0"""
         pass
 
-    def test_weight_prop(self):
-        test_animal = Animal()
-        test_animal.weight = 20
-        assert test_animal.weight == 20
-
-    def test_add_weight(self):
-        test_animal = Animal()
-        test_animal.weight = 20
-        test_animal.weight += 20
-        assert test_animal.weight == 40
-
-    def test_fitness_calc(self):
+    def test_fitness(self):
+        """ Checks if calling fitness without updating weight
+            or age will produce the same result."""
         test_animal = Animal()
         old = test_animal.fitness
         test_animal.feed(200)
         new = test_animal.fitness
         print(old, new)
-        assert new != old
+        assert new > old
 
-    def test_fitness_unchanged(self):
-        """ Checks if calling fitness without updating weight
-            or age will produce the same result."""
         test_animal = Animal()
         old = test_animal.fitness
         new = test_animal.fitness
         print(old, new)
         assert old == new
 
-    def test_reset_of_has_moved(self):
+    def test_age(self):
         test_animal = Animal()
-        test_animal._has_moved = True
+        test_animal.age = 20
+        assert test_animal.weight == 20
+        test_animal.weight += 20
+        assert test_animal.weight == 40
+        assert test_animal._compute_fitness
+
+    def test_weight(self):
+        test_animal = Animal()
+        test_animal.weight = 20
+        assert test_animal.weight == 20
+        test_animal.weight += 20
+        assert test_animal.weight == 40
+        assert test_animal._compute_fitness
+
+    def test_has_moved(self):
+        test_animal = Animal()
+        assert test_animal.has_moved is False
+        assert test_animal.has_moved is True
+
+    def test_reset_has_moved(self):
+        test_animal = Animal()
+        has_moved = test_animal.has_moved
+        assert not has_moved
+        assert test_animal._has_moved
         test_animal.reset_has_moved()
         assert not test_animal._has_moved
 
+    def test_will_migrate(self):
+        will_migrate = 0
+        for _ in range(1000):
+            test_animal = Animal()
+            test_animal._compute_fitness = False
+            test_animal._fitness = 1.0
+            if test_animal.will_migrate():
+                will_migrate += 1
+        assert 200 < will_migrate < 300
+
     def test_birth(self):
-        animal = Animal(10, 40)
-        value = animal.birth(10000)
-        print(value)
-        assert value != 0
+        animal = Animal(10, 60)
+        birth = animal.birth(10000)
+        assert isinstance(birth, Animal)
 
     def test_death(self):
         animal = Animal(50, 1.4)
@@ -112,11 +175,16 @@ class TestAnimal:
         for _ in range(100):
             assert animal_2.death()
 
-    def test_set_param(self, carnivore_parameters_right):
-        animal = Carnivore(30, 10)
-        animal.set_parameters(carnivore_parameters_right)
-        assert animal.eta == 0.03
-        assert animal.phi_age == 0.3
+    def test_feed(self):
+        test_animal = Animal()
+        assert test_animal.feed(0.1) == 0
+        assert test_animal.feed(1000) > 100
+
+    def test_lose_weight(self):
+        test_animal = Animal()
+        test_animal.weight = 100
+        test_animal.lose_weight()
+        assert test_animal.weight == 100 * 0.95
 
 
 class TestHerbivore:
@@ -151,6 +219,31 @@ class TestCarnivore:
         assert test_carnivore.fitness is not None
         assert 0 <= test_carnivore.fitness <= 1
 
+    def test_kill_or_not(self):
+        carnivore = Carnivore()
+        carnivore._compute_fitness = False
+        herbivore = Herbivore()
+        herbivore._compute_fitness = False
+        carnivore._fitness = 1
+        herbivore._fitness = 0.5
+        kills = 0
+        for _ in range(1000):
+            if carnivore.kill_or_not(herbivore):
+                kills += 1
+        assert 30 < kills < 70
+
+    def test_eat(self):
+        carnivore = Carnivore()
+        carnivore.weight = 30
+        carnivore.eat(1000, 0)
+        assert carnivore.weight == 30 + carnivore.F * carnivore.beta
+        carnivore.weight = 30
+        carnivore.eat(10, 0)
+        assert carnivore.weight == 30 + 10 * carnivore.beta
+        carnivore.weight = 30
+        carnivore.eat(1000, 45)
+        assert carnivore.weight == 30 + 5 * carnivore.beta
+
     def test_feed(self, herbivore_list):
         carnivore = Carnivore(5, 100)
         sorted_list = sorted(herbivore_list, key=lambda var: var.fitness)
@@ -163,5 +256,5 @@ class TestCarnivore:
         assert length_b < length_a
         assert weight_a < weight_b
         assert weight_b - weight_a <= carnivore.F*carnivore.beta
-
-
+        # Should add tests that changes DeltaPhiMax and checks
+        # that part of the code
